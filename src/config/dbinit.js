6f015bb3eb9616c91dfc74cff75e1bd3d79b9c04
AlexxDv <alexxdimov@gmail.com>
2023-01-30 19:50:08 +0100

    Database init

---
+++ src/config/dbinit.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const uri = "mongodb://127.0.0.1:27017/cubicle";
+
+async function initDatabase() {
+    await mongoose.connect(uri)
+
+    console.log("Database initialized");
+}
+
+module.exports = initDatabase;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const routes = require("./routes");
 const config = require("./config");
+const initDatabase = require("./config/dbinit");
 
 const app = express();
 // const setupViewEngine = require('./config/viewEngine')
@@ -13,6 +14,8 @@ app.use(express.static("src/public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
-app.listen(config.PORT, () =>
-    console.log(`Server is running on ${config.PORT}`)
-);
+initDatabase()
+    .then(() =>
+        app.listen(config.PORT, () =>
+            console.log(`Server is running on ${config.PORT}`)))
+    .catch((err) => console.error(err));
