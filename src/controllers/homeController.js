bbce79507c494944b95fd41cee9bf70450daf8ef
AlexxDv <alexxdimov@gmail.com>
2023-01-30 20:39:55 +0100

    search added

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,26 @@
-exports.getHomePage = (req, res) => {
-    res.render("index");
+const Cube = require('../models/Cube')
+
+
+
+exports.getHomePage = async (req, res) => {
+    const { search, from: difficultyFrom, to: difficultyTo } = req.query;
+
+    let cubes = await Cube.find()
+
+    if (search) {
+        cubes = cubes.filter(c => c.name.toLowerCase().includes(search.toLowerCase()))
+
+    }
+
+    if (difficultyFrom) {
+        cubes = cubes.filter(c => c.difficultyLevel >= difficultyFrom)
+    }
+
+    if (difficultyTo) {
+        cubes = cubes.filter(c => c.difficultyLevel <= difficultyTo)
+    }
+
+    res.render("index", { cubes, search, difficultyFrom, difficultyTo });
 };
 
 exports.getAboutPage = (req, res) => {
