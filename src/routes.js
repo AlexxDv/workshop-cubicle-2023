01bc07990c09fc9b28691da2655d73934e4218d1
AlexxDv <alexxdimov@gmail.com>
2023-02-01 18:31:37 +0100

    add new routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const cubeController = require("./controllers/cubeController");
 const homeController = require("./controllers/homeController");
+const accessoryControler = require("./controllers/accessoryController");
 
 router.get("/", homeController.getHomePage);
 router.get("/about", homeController.getAboutPage);
@@ -12,6 +13,9 @@ router.get('/404', homeController.getErrorPage);
 router.get('/create', cubeController.getCreateCube)
 router.post('/create', cubeController.postCreateCube)
 router.get('/details/:cubeId', cubeController.getDetails);
+
+// Друг начин за подаване на заявка ( "use" взима само началото на пътя ./accessories)
+router.use("/accessory", accessoryControler)
 
 
 module.exports = router;
