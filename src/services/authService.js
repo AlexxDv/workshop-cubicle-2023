5cb71675a703dc708a627c8565e1a28c85597484
AlexxDv <alexxdimov@gmail.com>
2023-02-03 18:35:46 +0100

    add lib

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,22 +1,29 @@
-const User = require('../models/User')
+
+const User = require('../models/User');
+const config = require('../config');
+const jwt = require('../lib/jsonwebtoken');
+
 
 exports.getUserByUserName = (username) => {
-    return User.findOne({ username })
-}
+    return User.findOne({ username });
+};
 
 exports.register = (username, password) => {
-    return User.create({ username, password })
-}
+    return User.create({ username, password });
+};
 
 exports.login = async (username, password) => {
-    const user = await this.getUserByUserName(username)
+    const user = await this.getUserByUserName(username);
 
-    const isValid = await user.validatePassword(password)
+    const isValid = await user.validatePassword(password);
 
     if (!user || !isValid) {
         throw "Invalid username or password";
-    }
+    };
+    const payload = { username: user.username };
+    const token = await jwt.sign(payload, config.SECRET, { expiresIn: "2h" });
 
-    return user;
+
+    return token;
 
 }
