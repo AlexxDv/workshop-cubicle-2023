74f2408f891cfb99c9785a2545d209c60e47e314
Aleksandar Dimov <alexxdimov@gmail.com>
2023-02-07 14:24:11 +0100

    username and pass validators

--- src/models/User.js
+++ src/models/User.js
@@ -1,34 +1,37 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
-
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
     username: {
         type: String,
         required: true,
-        minLength: 3,
+        minLength: 5,
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return /^[a-zA-Z0-9]+$/.test(v);
+            },
+            message: "Username must contain only letters and numbers",
+        },
     },
     password: {
         type: String,
         required: true,
-        minLength: [6, "Password is too short!"]
-
-    }
-})
+        minLength: [8, "Password is too short!"],
+        validate: /^[a-zA-Z0-9]+$/,
+    },
+});
 
 userSchema.pre("save", function (next) {
-    bcrypt.hash(this.password, 10)
-        .then(hash => {
-            this.password = hash
-            next()
-        })
+    bcrypt.hash(this.password, 10).then((hash) => {
+        this.password = hash;
+        next();
+    });
+});
 
-})
-
-userSchema.method('validatePassword', function (password) {
-    return bcrypt.compare(password, this.password)
-
-})
+userSchema.method("validatePassword", function (password) {
+    return bcrypt.compare(password, this.password);
+});
 
 const User = model("User", userSchema);
 
